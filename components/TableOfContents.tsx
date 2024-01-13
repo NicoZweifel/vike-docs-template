fc8bb5b5e5f9c25cdf6b0b2773631757099487eb
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-13 06:06:00 +0100

    fix: hide 'On this Page' if empty

--- components/TableOfContents.tsx
+++ components/TableOfContents.tsx
@@ -21,16 +21,18 @@ export function TableOfContents({
 
   return (
     <nav {...props} className={cn('flex px-2 py-4 flex-col gap-1', className)}>
-      <div className={'flex flex-row items-center gap-1'}>
-        <List size={16} />
-        <p
-          className={
-            'font-bold shrink-0 text-neutral-900 dark:text-neutral-100'
-          }
-        >
-          On this page
-        </p>
-      </div>
+      {components.length > 0 && (
+        <div className={'flex flex-row items-center gap-1'}>
+          <List size={16} />
+          <p
+            className={
+              'font-bold shrink-0 text-neutral-900 dark:text-neutral-100'
+            }
+          >
+            On this page
+          </p>
+        </div>
+      )}
       {components.map(({ title, Component, level }) => (
         <a
           key={title}

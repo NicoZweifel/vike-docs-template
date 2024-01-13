96b90341af4df5383d8e91f6d61b3500988f0a6b
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-13 06:04:15 +0100

    fix: category/parent anchors

--- components/Page/PageHeader.tsx
+++ components/Page/PageHeader.tsx
@@ -11,22 +11,32 @@ export function PageHeader({
   const {
     pageProps: { frontmatter },
   } = usePageContext();
-  const path = frontmatter.path.replace('/', '').replaceAll('/', ' / ');
-  const formatted = path.charAt(0).toUpperCase() + path.slice(1);
+  const path = frontmatter.path.split('/').filter((x) => x.length > 0);
   const Component =
     frontmatter.description && getMDXComponent(frontmatter.description);
 
+  let basePath = '';
+
   return (
     <header {...props} className={cn('gap-1 flex flex-col', className)}>
-      {formatted !== frontmatter.title && (
-        <p
-          className={
-            'font-semibold text-xl  text-neutral-600 dark:text-neutral-400'
-          }
-        >
-          {formatted}
-        </p>
-      )}
+      <div className={'flex flex-row gap-1'}>
+        {path.map((x, i) => {
+          basePath = `${basePath}/${x}`;
+          return (
+            <a key={x} href={basePath}>
+              <p
+                className={
+                  'font-semibold text-xl text-neutral-600 hover:text-neutral-800 dark:text-neutral-400 dark:hover:text-neutral-300'
+                }
+              >
+                {x.charAt(0).toUpperCase() +
+                  x.slice(1) +
+                  (i === path.length - 1 ? '' : ' / ')}
+              </p>
+            </a>
+          );
+        })}
+      </div>
       <p className={'font-bold text-4xl'}>{frontmatter.title}</p>
       {frontmatter.description && (
         <Component

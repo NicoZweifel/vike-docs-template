50ae5d801beba8cf1ee5e75e5e65bfd1a82579d2
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-22 22:25:53 +0100

    fix: format, move state out of component

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,11 +1,19 @@
 import { usePageContext } from '../context/PageContext';
 import { ComponentProps } from 'preact';
+import { cn } from '../utils/cn';
 
-export function Link(props: ComponentProps<'a'>) {
+export function Link({ className, ...props }: ComponentProps<'a'>) {
   const { urlPathname } = usePageContext();
-  const className = [props.className, urlPathname === props.href && 'is-active']
-    .filter(Boolean)
-    .join(' ');
 
-  return <a {...props} className={className} />;
+  return (
+    <a
+      {...props}
+      className={cn(
+        className,
+        urlPathname === props.href
+          ? 'bg-neutral-300/40 dark:bg-neutral-700/20'
+          : undefined
+      )}
+    />
+  );
 }
--- components/NavNode.tsx
+++ components/NavNode.tsx
@@ -2,55 +2,86 @@ import { ComponentProps } from 'preact';
 import { cn } from '../utils/cn';
 import { NavTreeNode } from '../types/NavTreeNode';
 import { usePageContext } from '../context/PageContext';
+import { Link } from './Link';
+import { useMemo } from 'preact/hooks';
+
+type NavNodeProps = {
+  node: NavTreeNode;
+  flex?: 'col' | 'row';
+};
+
+const useNavNode = ({ node, flex }: NavNodeProps) => {
+  const {
+    pageProps: { baseRoute },
+  } = usePageContext();
+
+  const { children, path, navItems } = node;
+
+  const isBaseRoute = (path.length === 0 ? '/' : path) === baseRoute;
+  const childNodes = useMemo(
+    () =>
+      children
+        .filter((x) => x.navItems.length > 0)
+        .map((x) => (
+          <NavNode
+            key={x.name}
+            node={x}
+            flex={!isBaseRoute ? flex : undefined}
+          />
+        )),
+    [children, flex, isBaseRoute]
+  );
+
+  const content = useMemo(
+    () =>
+      navItems.length > 0 && (
+        <div
+          className={cn(
+            !isBaseRoute
+              ? 'ml-1 pl-2 border-l border-neutral-300/60 dark:border-neutral-800/40'
+              : undefined,
+            `flex flex-${flex ?? 'col'} gap-1`
+          )}
+        >
+          {
+            <div className={`flex flex-col gap-1`}>
+              {navItems.map(({ title, route }) => (
+                <Link
+                  key={route}
+                  className={
+                    'whitespace-nowrap text-sm py-1 px-2 flex items-center hover:bg-neutral-200/80 dark:hover:bg-neutral-800/60 rounded text-neutral-600 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300 font-semibold'
+                  }
+                  href={route}
+                >
+                  {title}
+                </Link>
+              ))}
+            </div>
+          }
+          {childNodes}
+        </div>
+      ),
+    [childNodes, flex, isBaseRoute, navItems]
+  );
+
+  return useMemo(
+    () => ({
+      isBaseRoute,
+      content,
+      path,
+    }),
+    [content, isBaseRoute, path]
+  );
+};
 
 export const NavNode = ({
   node,
   className,
   flex,
   ...props
-}: { node: NavTreeNode; flex?: 'col' | 'row' } & Omit<
-  ComponentProps<'div'>,
-  'children'
->) => {
-  const {
-    pageProps: { baseRoute },
-  } = usePageContext();
-  const { name, children, path, navItems } = node;
-
-  const isBaseRoute = (path.length === 0 ? '/' : path) === baseRoute;
-  const childNodes = children
-    .filter((x) => x.navItems.length > 0)
-    .map((x) => (
-      <NavNode key={x.name} node={x} flex={!isBaseRoute ? flex : undefined} />
-    ));
-
-  const content = navItems.length > 0 && (
-    <div
-      className={cn(
-        !isBaseRoute
-          ? 'ml-1 pl-2 border-l border-neutral-300/60 dark:border-neutral-800/40'
-          : undefined,
-        `flex flex-${flex ?? 'col'} gap-1`
-      )}
-    >
-      {
-        <div className={`flex flex-col gap-1`}>
-          {navItems.map(({ title, route }) => (
-            <a
-              key={route}
-              className={
-                'whitespace-nowrap text-sm py-1 px-2 flex items-center hover:bg-neutral-200/80 dark:hover:bg-neutral-800/60 rounded text-neutral-600 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300 font-semibold'
-              }
-              href={route}
-            >
-              {title}
-            </a>
-          ))}
-        </div>
-      }
-      {childNodes}
-    </div>
-  );
+}: NavNodeProps & Omit<ComponentProps<'div'>, 'children'>) => {
+  const { name, path } = node;
+  const { isBaseRoute, content } = useNavNode({ node, flex });
 
   return isBaseRoute ? (
     content
@@ -60,14 +91,14 @@ export const NavNode = ({
       className={cn(`flex flex-${flex ?? 'col'} gap-1`, className)}
     >
       <>
-        <a
+        <Link
           href={path}
           className={
             'text-sm font-bold text-start py-1 pl-2  hover:bg-neutral-200/80 dark:hover:bg-neutral-800/60 rounded text-neutral-600 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300'
           }
         >
           {name.charAt(0).toUpperCase() + name.slice(1)}
-        </a>
+        </Link>
         {content}
       </>
     </div>

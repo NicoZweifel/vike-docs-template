9013dfe73046471b225489f6157ef0819d46508e
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-23 00:37:04 +0100

    feat: indicate active parent nav nodes

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,18 +1,31 @@
 import { usePageContext } from '../context/PageContext';
 import { ComponentProps } from 'preact';
 import { cn } from '../utils/cn';
+import { useMemo } from 'preact/hooks';
 
 export function Link({ className, ...props }: ComponentProps<'a'>) {
   const { urlPathname } = usePageContext();
+
+  const isParent = useMemo(
+    () => props.href !== '/' && urlPathname.startsWith(props.href.toString()),
+    [props.href, urlPathname]
+  );
+
+  const isActive = useMemo(
+    () => urlPathname === props.href,
+    [props.href, urlPathname]
+  );
 
   return (
     <a
       {...props}
       className={cn(
         className,
-        urlPathname === props.href
+        isActive
           ? 'bg-neutral-200/80 dark:bg-neutral-800/60 text-neutral-900 dark:text-neutral-300 '
-          : undefined
+          : isParent
+            ? 'bg-neutral-200/40 dark:bg-neutral-800/30 text-neutral-900 dark:text-neutral-300 '
+            : undefined
       )}
     />
   );

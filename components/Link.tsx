8f1ed9759e12ade8929e9e1de50880a4641c45b2
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-23 01:38:09 +0100

    fix: active parent nav item text color

--- components/Link.tsx
+++ components/Link.tsx
@@ -27,9 +27,9 @@ export function Link({ className, href, ...props }: ComponentProps<'a'>) {
       className={cn(
         className,
         isActive
-          ? 'font-semibold bg-neutral-200/80 dark:bg-neutral-800/60 text-neutral-900 dark:text-neutral-300 '
+          ? 'font-semibold bg-neutral-200/80 dark:bg-neutral-800/60 text-neutral-900 dark:text-neutral-300'
           : isParent
-            ? 'font-semibold'
+            ? 'font-semibold text-neutral-900 dark:text-neutral-300'
             : undefined
       )}
     />

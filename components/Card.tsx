0e027574e3f9deeb983c68df3c3c2b7af57ef87f
Nico Zweifel <34443492+NicoZweifel@users.noreply.github.com>
2024-01-24 12:17:43 +0100

    feat: improve cards, linkbutton

--- components/Card.tsx
+++ components/Card.tsx
@@ -23,31 +23,36 @@ export const Card = ({
   switch (variant) {
     case 'success':
       x = {
-        className: 'bg-green-300/10 border-green-500/10',
+        className:
+          'text-green-800/90 dark:text-green-300/90 bg-green-300/10 border-green-500/10',
         icon: <CheckSquare size={size} />,
       };
       break;
     case 'warning':
       x = {
-        className: 'bg-amber-300/10 border-amber-500/10',
+        className:
+          'text-amber-500/90 dark:text-amber-300/80 bg-amber-300/10 border-amber-500/10',
         icon: <AlertTriangle size={size} />,
       };
       break;
     case 'info':
       x = {
-        className: 'bg-blue-300/10 border-blue-500/10',
+        className:
+          'text-blue-900/90 dark:text-blue-300/80 bg-blue-300/10 border-blue-500/10',
         icon: <InfoIcon size={size} />,
       };
       break;
     case 'error':
       x = {
-        className: 'bg-red-300/10 border-red-500/10',
+        className:
+          'text-red-700/90 dark:text-red-400/90 bg-red-300/10 border-red-500/10',
         icon: <XCircle size={size} />,
       };
       break;
     default:
       x = {
-        className: 'bg-neutral-500/20 border-neutral-500/20',
+        className:
+          'bg-neutral-500/20 border-neutral-500/20 text-neutral-950/80 dark:text-neutral-100/40 ',
       };
       break;
   }
@@ -55,7 +60,7 @@ export const Card = ({
     <div
       {...p}
       className={cn(
-        'flex flex-row gap-4 border px-4 py-4 rounded place-items-center text-neutral-900/90 dark:text-neutral-400/90',
+        'flex flex-row gap-4 border px-4 py-4 rounded place-items-center',
         x.className,
         className
       )}
